4d48683c1f4adf54c727cc542f39a97e3596dafb
Ruby Tsai <hjtsai0307@gmail.com>
2021-07-14 12:02:58 +0800

    Move create button and add sort dropdown list

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,10 +4,21 @@ const router = express.Router()
 // 引用 Todo model
 const Restaurant = require('../../models/restaurant')
 
+// Sort & Filter
+router.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .sort({ name: 'asc' })
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
+})
+
+
 // Index
 router.get('/', (req, res) => {
   Restaurant.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
