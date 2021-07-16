9897b24d69da8192755076f83d2feedca2c52458
Ruby Tsai <hjtsai0307@gmail.com>
2021-07-16 10:35:27 +0800

    Add db close to seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -3,19 +3,31 @@ const db = require('../../config/mongoose')
 
 const restaurantList = require('../../restaurant.json')
 
+// 助教建議的寫法
+// 不需要for loop, 因為 MongoDB 在 Create 時，如果提供陣列會自動在內部進行 for 迴圈處理。
 db.once('open', () => {
-  for (let i = 0; i < 8; i++) {
-    Restaurant.create({
-      id: restaurantList.results[i].id,
-      name: restaurantList.results[i].name,
-      image: restaurantList.results[i].image,
-      category: restaurantList.results[i].category,
-      rating: restaurantList.results[i].rating,
-      location: restaurantList.results[i].location,
-      google_map: restaurantList.results[i].google_map,
-      phone: restaurantList.results[i].phone,
-      description: restaurantList.results[i].description
+  Restaurant.create(restaurantList.results)
+    .then(() => {
+      db.close();  // 新增
+      console.log('done')
     })
-  }
-  console.log('done')
 })
+
+// 我原始的寫法，加上async/await
+// db.once('open', async () => {
+//   for (let i = 0; i < 8; i++) {
+//     await Restaurant.create({
+//       id: restaurantList.results[i].id,
+//       name: restaurantList.results[i].name,
+//       image: restaurantList.results[i].image,
+//       category: restaurantList.results[i].category,
+//       rating: restaurantList.results[i].rating,
+//       location: restaurantList.results[i].location,
+//       google_map: restaurantList.results[i].google_map,
+//       phone: restaurantList.results[i].phone,
+//       description: restaurantList.results[i].description
+//     })
+//   }
+//   db.close()
+//   console.log('seeder done!')
+// })
