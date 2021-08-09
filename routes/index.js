bb37ab32f1e7ebe83257fccd1e7b71d33e319036
Ruby Tsai <hjtsai0307@gmail.com>
2021-08-09 11:04:12 +0800

    feat: add user schema and login page

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
-// 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
 
-// 引入 home 模組程式碼
 const home = require('./modules/home')
+const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
+
+router.use('/users', users)
+router.use('/restaurants', restaurants)
 router.use('/', home)
 
-// 引入 restaurants 模組程式碼
-const restaurants = require('./modules/restaurants')
-router.use('/restaurants', restaurants)
-
-// 匯出路由器
 module.exports = router
